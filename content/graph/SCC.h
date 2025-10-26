a9a3e87976b9b499a80c491f9664277c897a6d92
Zeyu Nie <znie2@u.rochester.edu>
2025-10-26 00:24:50 -0400

    SCC

--- content/graph/SCC.h
+++ content/graph/SCC.h
@@ -3,39 +3,44 @@
  * Date: 2009-10-28
  * License: CC0
  * Source: Czech graph algorithms book, by Demel. (Tarjan's algorithm)
- * Description: Finds strongly connected components in a
+ * Description: Finds strongly connected components in a 0-indexed 
  * directed graph. If vertices $u, v$ belong to the same component,
- * we can reach $u$ from $v$ and vice versa.
- * Usage: scc(graph, [\&](vi\& v) { ... }) visits all components
- * in reverse topological order. comp[i] holds the component
- * index of a node (a component only has edges to components with
- * lower index). ncomps will contain the number of components.
+ * color[u] = color[v].
  * Time: O(E + V)
  * Status: Bruteforce-tested for N <= 5
  */
 #pragma once
 
-vi val, comp, z, cont;
-int Time, ncomps;
-template<class G, class F> int dfs(int j, G& g, F& f) {
-	int low = val[j] = ++Time, x; z.push_back(j);
-	for (auto e : g[j]) if (comp[e] < 0)
-		low = min(low, val[e] ?: dfs(e,g,f));
+struct Kosaraju {
+    int n, sccCnt;
+    vector<vi> g, g2;
+    vi s, color;
+    vector<bool> vis;
 
-	if (low == val[j]) {
-		do {
-			x = z.back(); z.pop_back();
-			comp[x] = ncomps;
-			cont.push_back(x);
-		} while (x != j);
-		f(cont); cont.clear();
-		ncomps++;
-	}
-	return val[j] = low;
-}
-template<class G, class F> void scc(G& g, F f) {
-	int n = sz(g);
-	val.assign(n, 0); comp.assign(n, -1);
-	Time = ncomps = 0;
-	rep(i,0,n) if (comp[i] < 0) dfs(i, g, f);
-}
+    Kosaraju(int n) : n(n), g(n), g2(n), color(n), vis(n) {}
+
+    void addEdge(int u, int v) {
+        g[u].push_back(v);
+        g2[v].push_back(u);
+    }
+
+    void dfs1(int u) {
+        vis[u] = true;
+        for (int v : g[u]) if (!vis[v]) dfs1(v);
+		s.push_back(u);
+    }
+
+    void dfs2(int u) {
+        color[u] = sccCnt;
+        for (int v : g2[u]) if (!color[v]) dfs2(v);
+    }
+
+    void work() {
+        sccCnt = 0;
+        rep(i, 0, n) if (!vis[i]) dfs1(i);
+        for (int i = n - 1; i >= 0; --i) if (!color[s[i]]) {
+			++sccCnt;
+			dfs2(s[i]);
+		}
+    }
+};
